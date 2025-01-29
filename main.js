29b6f155d8aa1dcd32fc469f09a4a1196c8e96b4
ki3mi <ccentemejiajosecarlos@gmail.com>
2025-01-29 00:37:03 -0500

    almacenamiento en localStorage

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const taskForm = document.getElementById("taskForm")
 const taskList = document.getElementById("taskList")
+mapTasksInLocalStorage()
 
+
+
+// evento para Agregar tareas
 taskForm.addEventListener("submit", (event)=>{
     event.preventDefault()
-    const taskInput = document.getElementById("input")    
-    taskList.insertAdjacentHTML("afterbegin",
-        "<div class='task'>"
-            +"<p contenteditable = 'true'>"+taskInput.value+"</p>"
-            +"<section>"
-                +"<button id='delete' class='btn'>Eliminar</button>"
-            +"</section>"                    
-        +"</div>")
+    const taskInput = document.getElementById("input")
+    const text = taskInput.value
+    if (taskInput.value.trim() === ""){
+        alert("Es necesario agregar un texto")
+    }else{
+        // REVISAR SI ES MEJOR CON ESTE MÉTODO O LLAMANDO A LA FUNCIÓN DE MOSTRAR LAS TAREAS
+        const tasks = getTaskInLocalStorage()
+        addTask(text, tasks.length)
+        storeTaskInLocalStorage(text)
+    }
     taskInput.value = ""
 })
 
@@ -21,6 +27,54 @@ taskList.addEventListener("click", (event) =>{
     const type = event.target.id
     if (type === "delete"){
         const task = clickElement.closest("div")
-        task.remove()
+        const id = task.id
+        deleteTaskInLocalStorage(id)
     }
 })
+
+
+
+
+
+//  **** FUNCTIONS ****  //
+
+// Agregar una tarea
+function addTask(task, id){
+    taskList.insertAdjacentHTML("afterbegin",
+        "<div id='"+id+"' class='task'>"
+            +"<p contenteditable = 'true'>"+task+"</p>"
+            +"<section>"
+                +"<button id='delete' class='btn'>Eliminar</button>"
+            +"</section>"                    
+        +"</div>")
+}
+
+// Obtener las tareas del localStorage
+function getTaskInLocalStorage(){
+    const tasks = JSON.parse(localStorage.getItem("tasks") || "[]")
+    return tasks
+}
+
+// Mostrar las tareas guardadas en el local storage
+function mapTasksInLocalStorage(){
+    taskList.innerHTML = ""
+    const tasks = getTaskInLocalStorage()
+    for (task in tasks){
+        addTask(tasks[task], task)
+    }
+}
+
+// Agregar las tareas al localStorage
+function storeTaskInLocalStorage(task){
+    const tasks = getTaskInLocalStorage()
+    tasks.push(task)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+// Eliminar tarea del localStorage
+function deleteTaskInLocalStorage(id){
+    const tasks = getTaskInLocalStorage()
+    tasks.splice(id, 1)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    mapTasksInLocalStorage()
+}
